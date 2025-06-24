173572b00ec5289869cf6940684325c29b5c8a2e
Kadu <kadu.silva2014@gmail.com>
2025-06-24 19:14:34 -0300

    feat: adicionar variáveis de ambiente JWT_SECRET e DATABASE_URL no arquivo env.ts e remover arquivo de rotas index.ts

--- src/env.ts
+++ src/env.ts
@@ -4,5 +4,7 @@ export const env = z
 	.object({
 		PORT: z.coerce.number().default(3000),
 		URL_CORS: z.string().default("*"),
+		JWT_SECRET: z.string().default("your-secret-key"),
+		DATABASE_URL: z.string().url().default("postgres://user:password@localhost:5432/dbname"),
 	})
 	.parse(process.env);
