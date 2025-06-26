921f55c5d90913a64fb102052733427421b7eb7f
Kadu <kadu.silva2014@gmail.com>
2025-06-26 18:59:15 -0300

    feat: adicionar middleware de autenticação com verificação de token JWT

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+
+import { env } from "@/env";
+import { NotFoundError, UnauthorizedError } from "@/helpers/http-errors";
+
+export default function authMiddleware() {
+	return (req: Request, _res: Response, next: NextFunction) => {
+		const token = req.headers.authorization?.split(" ").at(-1);
+		if (!token) throw new NotFoundError("Token not found");
+
+		let payload: jwt.JwtPayload | string;
+
+		try {
+			payload = jwt.verify(token, env.JWT_SECRET);
+		} catch {
+			throw new UnauthorizedError("Invalid token");
+		}
+
+		if (typeof payload === "string") {
+			throw new UnauthorizedError("Invalid token");
+		}
+
+		if (!payload.sub) {
+			throw new UnauthorizedError("Invalid token");
+		}
+
+		req.user = {
+			id: payload.sub
+		}
+
+		return next();
+	};
+}
