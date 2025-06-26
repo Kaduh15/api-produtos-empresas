552e6fff740c1731d0d29ec468b70e69e7847e72
Kadu <kadu.silva2014@gmail.com>
2025-06-26 13:15:14 -0300

    feat: mover schema de autenticação para o arquivo de índice e reestruturar a definição

--- src/routes/auth/schemas/index.ts
+++ src/routes/auth/schemas/index.ts
@@ -1,1 +1,8 @@
-export * from "./authLogin.schema";
+import { z } from 'zod'
+
+export const authLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+export type AuthLoginSchema = z.infer<typeof authLoginSchema>
